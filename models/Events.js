1468083db992034e9484c7a262a0f1c99b692d02
deanban <pbp2005@gmail.com>
2019-08-03 20:48:49 -0400

    setup event schema

---
+++ models/Events.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const EventSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+
+    desc: {
+      type: String,
+      required: true
+    },
+
+    address: {
+      type: String,
+      required: true
+    },
+
+    hostedBy: {
+      type: String,
+      required: true
+    },
+
+    startsAt: {
+      type: Date,
+      required: true
+    },
+
+    endsAt: {
+      type: Date,
+      required: true
+    },
+
+    photo: {
+      data: Buffer,
+      type: String
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('events', EventSchema);
