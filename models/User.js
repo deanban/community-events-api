ae6cc16ecc064f27d04d2f7200848d758397c902
deanban <pbp2005@gmail.com>
2019-08-03 20:51:34 -0400

    update user schema

--- models/User.js
+++ models/User.js
@@ -2,40 +2,38 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  placeOfWork: [
-    {
-      name: {
-        type: String,
-        required: true
-      },
-      address: {
-        type: String,
-        required: true
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    placeOfWork: [
+      {
+        name: {
+          type: String,
+          required: true
+        },
+        address: {
+          type: String,
+          required: true
+        }
       }
+    ],
+    photo: {
+      data: Buffer,
+      type: String
     }
-  ],
-  photo: {
-    data: Buffer,
-    type: String
   },
-
-  date: {
-    type: Date,
-    default: Date.now()
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model('users', UserSchema);
